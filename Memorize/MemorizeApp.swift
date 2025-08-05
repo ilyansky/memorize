8e58ca8f2d25bcbbdda77e98396462426de4fbfc
ilyansky <prophetsatin@gmail.com>
2025-08-06 01:02:26 +0300

    init commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Satin on 06.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
