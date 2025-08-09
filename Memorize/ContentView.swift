25f80ae66626f35ed22fdc8b831259147c98e8f1
ilyansky <prophetsatin@gmail.com>
2025-08-09 16:42:36 +0300

    temp State

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,17 +7,62 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.mint)
         .padding()
     }
+
 }
+
+struct CardView: View {
+
+    @State var isFaceUp: Bool = false
+    
+    var body: some View {
+
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 5)
+                Text("👻").font(.largeTitle)
+            } else {
+                base
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+            print(1)
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
